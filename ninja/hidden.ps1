bd1476b2d80f37e5fcf8357417a2dccd37e8e329
ธนาวัล <152389095+thanawan230653@users.noreply.github.com>
2025-11-30 11:33:46 +0700

    Update hidden.ps1

--- ninja/hidden.ps1
+++ ninja/hidden.ps1
@@ -1,10 +1,48 @@
 Write-Host "Running NINJA Secure Unlock..." -ForegroundColor Green
 
 # ==== ตัวอย่างคำสั่งจริง (คุณแก้ได้เลย) ====
-fastboot devices
-fastboot oem unlock
+
+#start http://pawarit7.sytes.net/
+#exit
+cls
+@echo off
+cd bin
+s.txt
+mode con lines=12 cols=35
+color 6f
+cls
+title TOOL_T3AMX3_SKWAMX3 Serial
+:main
+cls
+echo.
+echo.
+echo.
+ECHO 1 - SK/T3 EDIT Serial
+ECHO 2 - FIX T3AMX3 Usb bring Mode
+ECHO 3 - EXIT
+ECHO.
+SET /P M=Type 1, 2 ENTER:
+IF %M%==1 GOTO op1
+IF %M%==2 GOTO FX
+IF %M%==3 GOTO exit
+
+:op1
+
+update bulkcmd "keyman write usid str 0000000000"
+update bulkcmd "keyman write usid str 1234567890"
+update bulkcmd "keyman read usid"
+update bulkcmd "saveenv"
+update bulkcmd "reset"
+echo 
+goto main
+
+:FX
 fastboot flashing unlock
-fastboot flash lk lk.bin
-fastboot flash boot boot.img
+fastboot getvar all
+fastboot oem update
+goto main
+
+# edit by pawarit
+
 
 Write-Host "Secure unlock done." -ForegroundColor Yellow
