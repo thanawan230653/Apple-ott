39662b5f7cf8aa8ef4a440a06fddc0afb557bbd4
ธนาวัล บุณสอน <152389095+thanawan230653@users.noreply.github.com>
2025-12-26 21:18:00 +0700

    Add command to erase bootloader in unlock.ps1

--- unlock.ps1
+++ unlock.ps1
@@ -29,6 +29,7 @@ function Do-Op1 {
 ./bin/update bulkcmd "keyman write usid str 24154048420000"
 ./bin/update bulkcmd "keyman read usid"
 ./bin/update bulkcmd "saveenv"
+./bin/update bulkcmd erase bootloader
 ./bin/update bulkcmd "reset"
     Write-Host ""
     Read-Host "Enter Goto Manu"
